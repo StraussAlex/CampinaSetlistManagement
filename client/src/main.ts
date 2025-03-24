e60ac3c78821aefeb6db52b949f1c0bf5bfe278c
Alexander Strauss <s2310238069@fhooe.at>
2025-03-24 15:23:54 +0100

    implemented vue-router and implemented client/src/services/api.ts for streamlining axios calls

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import songsOverview from './components/songsOverview.vue'
+import eventOverview from './components/eventOverview.vue'
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'events', component: eventOverview },
+        { path: '/songs', name: 'events', component: songsOverview },
+    ]
+});
+
+const app = createApp(App)
+
+app.use(router);
+app.mount('#app')
+//createApp(App).mount('#app')
