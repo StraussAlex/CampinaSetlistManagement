3e302169e52ac7977bf4f4d55f9c0ae97d4d2959
Maksym Zinchenko <awesome20051969@gmail.com>
2025-04-08 16:18:46 +0200

    Löschen und Anzeigen von Events hinzugefügt

--- client/src/components/eventOverview.vue
+++ client/src/components/eventOverview.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
+import Event from '../models/Event';
+import api from '../services/api';
 
 const router = useRouter();
 
@@ -13,10 +16,43 @@ function viewSongs(): void {
 function createEvent(): void {
     router.push("/create-event");
 }
+
+const EVENTS_API = "events";
+const events = ref<Event[]>([])
+
+async function loadEvents(): Promise<void> {
+    try{
+        const response = await api.get(EVENTS_API);
+        events.value = response.data;
+    } catch(error) {
+        console.log("An error in showing the events has appeared: " + error);
+    }
+}
+
+async function deleteEvent(eventId: number): Promise<void>{
+    try{
+        await api.delete(`${EVENTS_API}/${eventId}`);
+        events.value = events.value.filter(event => event._id !== eventId);
+        
+    } catch(error){
+        console.log("An error in deleting the event has appeared: " + error)
+    }
+}
+
+function confirmDelete(): void{
+    // TODO: ask to confirm the deletion of an event after clicking "delete" button
+}
+
+onMounted(() => loadEvents());
+
 </script>
 
 <template>
-    <p>Event list here</p>
+    <h1>Events</h1>
+    <ul v-if="events.length !== 0">
+        <li v-for="event in events">{{ event.name }} | {{ event.day }} of {{ event.month }} | {{ event.place }} <button>Details</button><button @click="deleteEvent(event._id)">Delete</button></li>
+    </ul>
+    <p v-else>No events are created yet</p>
     <p>This page could be the public landing page where people can see events?</p>
     <p>Band members could also log in here to maybe unlock more actions etc..?</p>
     <button>Log in</button>
