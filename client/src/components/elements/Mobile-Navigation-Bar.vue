068c2e938a92e047d5b56c3df91b5d011363eb97
Maksym Zinchenko <awesome20051969@gmail.com>
2025-05-08 11:22:19 +0200

    isActive-feature is added to MobileNavBar

--- client/src/components/elements/Mobile-Navigation-Bar.vue
+++ client/src/components/elements/Mobile-Navigation-Bar.vue
@@ -1,28 +1,47 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 import '/src/stylesheets/nav.css';
 
 const router = useRouter();
+const route = useRoute();
+
 function navigateTo(path: string): void {
-    router.push(path);
+    setTimeout(() => {
+      router.push(path);
+    }, 300); 
 }
+
 </script>
 
 <template>
-    <div class="nav-limiter"></div>
     <div class="mobile-navigation-bar">
-        <div class = "icon-container">
-            <img class = "home" src="../temporary-images/home.png" @click="navigateTo('/events')"></img>
-            <div class = "ani_square"></div>
-        </div>
-        <div class = "icon-container">
-            <img class = "setlists" src="../temporary-images/list.png" @click="navigateTo('/setlists')"></img>
-            <div class = "ani_square"></div>
-        </div>
-        <div class = "icon-container">
-        <img class = "tracks" src="../temporary-images/music.png" @click="navigateTo('/songs')"></img>
-        <div class = "ani_square"></div>
-        </div>
-    </div>
+  <div 
+    class="icon-container" 
+    @click="navigateTo('/events')" 
+    :class="{ active: route.name === 'events' }"
+  >
+    <img src="../temporary-images/home.png" />
+    <div class="ani_square"></div>
+  </div>
+
+  <div 
+    class="icon-container" 
+    @click="navigateTo('/setlists')" 
+    :class="{ active: route.name === 'setlists' }"
+  >
+    <img class="setlists" src="../temporary-images/list.png" />
+    <div class="ani_square"></div>
+  </div>
+
+  <div 
+    class="icon-container" 
+    @click="navigateTo('/songs')" 
+    :class="{ active: route.name === 'songs' }"
+  >
+    <img class="tracks" src="../temporary-images/music.png" />
+    <div class="ani_square"></div>
+  </div>
+</div>
+
 </template>
