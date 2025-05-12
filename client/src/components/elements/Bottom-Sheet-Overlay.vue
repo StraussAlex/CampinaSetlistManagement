55f5b847e22041f9994b236f295a15efc9b0ac02
Maksym Zinchenko <awesome20051969@gmail.com>
2025-05-12 11:30:46 +0200

    bottom-sheet-overlay ist färblich angepasst

--- client/src/components/elements/Bottom-Sheet-Overlay.vue
+++ client/src/components/elements/Bottom-Sheet-Overlay.vue
@@ -2,12 +2,28 @@
 <script setup lang="ts">
 import VueBottomSheet from "@webzlodimir/vue-bottom-sheet";
 import "@webzlodimir/vue-bottom-sheet/dist/style.css";
-import { ref, defineExpose } from "vue";
+import { ref, defineExpose, onMounted, watchEffect } from "vue";
 
 const myBottomSheet = ref<InstanceType<typeof VueBottomSheet>>();
 
 const open = () => myBottomSheet.value?.open();
 const close = () => myBottomSheet.value?.close();
+
+onMounted(() => {
+  const el = document.querySelector('.bottom-sheet__content');
+  watchEffect(() => {
+  if (el) {
+    (el as HTMLElement).style.backgroundColor = 'var(--background)';
+  }
+  })
+  
+
+  const thumb = document.querySelector('.bottom-sheet__draggable-thumb');
+  if( thumb) {
+    (thumb as HTMLElement).style.width = "80px";
+    (thumb as HTMLElement).style.height = "8px";
+  }
+});
 
 defineExpose({ open, close });
 </script>
@@ -15,17 +31,10 @@ defineExpose({ open, close });
 <template>
   <vue-bottom-sheet ref="myBottomSheet">
     <h1>Lorem Ipsum</h1>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
-    <p>Some content here</p>
+    <h2>What is Lorem Ipsum?</h2>
+    <p>
+      <strong>Lorem Ipsum</strong> is simply dummy text
+    </p>
   </vue-bottom-sheet>
 </template>
 
