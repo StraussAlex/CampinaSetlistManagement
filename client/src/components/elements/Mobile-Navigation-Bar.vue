ddb06bbe876722a36cf32a9dd044c2eb8866c2b3
Maksym Zinchenko <awesome20051969@gmail.com>
2025-05-13 10:48:27 +0200

    Mobile-Nav-Nar ist vervollständigt

--- client/src/components/elements/Mobile-Navigation-Bar.vue
+++ client/src/components/elements/Mobile-Navigation-Bar.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router';
-
+import { onMounted, ref } from 'vue';
+import api from '../../services/api'
 import '/src/stylesheets/nav.css';
-import { watchEffect } from 'vue';
 
 const router = useRouter();
 const route = useRoute();
@@ -10,6 +10,14 @@ const route = useRoute();
 function navigateTo(path: string): void {
   router.push(path);
 }
+
+const user = ref<any|null>(null);
+
+onMounted(async() => {
+    const response = await api.get('/auth', { withCredentials: true });
+    user.value  = response.data.user;
+    console.log(user);
+});
 
 </script>
 
@@ -40,6 +48,16 @@ function navigateTo(path: string): void {
     :class="{ active: route.name === 'songs' }"
   >
     <img class="tracks" src="../temporary-images/music.png" />
+    <div class="ani_square"></div>
+  </div>
+
+  <div 
+    class="icon-container" 
+    v-if="user !== null && user.isAdmin" 
+    @click="navigateTo('/manage-users')" 
+    :class="{ active: route.name === 'admin-page' }"
+  >
+    <img class="users" src="../temporary-images/profile-user.png" />
     <div class="ani_square"></div>
   </div>
 </div>
