f3e46a428566f2acfa98e9faab8675ec0e3f869d
Maksym Zinchenko <awesome20051969@gmail.com>
2025-05-14 19:39:02 +0200

    Mobile-NavBar-Border jetzt ist "border:top", sondern nicht "div"

--- client/src/components/elements/Mobile-Navigation-Bar.vue
+++ client/src/components/elements/Mobile-Navigation-Bar.vue
@@ -32,52 +32,51 @@ onMounted(async() => {
 </script>
 
 <template>
-  <div class = "nav-limiter"></div>
-    <div class="mobile-navigation-bar">
-  <div 
-    class="icon-container" 
-    @click="navigateTo('/events')" 
-    :class="{ active: route.name === 'events' }"
-  >
-    <img src="../temporary-images/home.png" />
-    <div class="ani_square"></div>
-  </div>
+  <div class="mobile-navigation-bar">
+    <div 
+      class="icon-container" 
+      @click="navigateTo('/events')" 
+      :class="{ active: route.name === 'events' }"
+    >
+      <img src="../temporary-images/home.png" />
+      <div class="ani_square"></div>
+    </div>
 
-  <div 
-    class="icon-container" 
-    @click="navigateTo('/setlists')" 
-    :class="{ active: route.name === 'setlists' }"
-  >
-    <img class="setlists" src="../temporary-images/list.png" />
-    <div class="ani_square"></div>
-  </div>
+    <div 
+      class="icon-container" 
+      @click="navigateTo('/setlists')" 
+      :class="{ active: route.name === 'setlists' }"
+    >
+      <img class="setlists" src="../temporary-images/list.png" />
+      <div class="ani_square"></div>
+    </div>
 
-  <div 
-    class="icon-container" 
-    @click="navigateTo('/songs')" 
-    :class="{ active: route.name === 'songs' }"
-  >
-    <img class="tracks" src="../temporary-images/music.png" />
-    <div class="ani_square"></div>
-  </div>
+    <div 
+      class="icon-container" 
+      @click="navigateTo('/songs')" 
+      :class="{ active: route.name === 'songs' }"
+    >
+      <img class="tracks" src="../temporary-images/music.png" />
+      <div class="ani_square"></div>
+    </div>
 
-  <div 
-    class="icon-container" 
-    v-if="user !== null && user.isAdmin" 
-    @click="navigateTo('/manage-users')" 
-    :class="{ active: route.name === 'admin-page' }"
-  >
-    <img class="users" src="../temporary-images/profile-user.png" />
-    <div class="ani_square"></div>
-  </div>
+    <div 
+      class="icon-container" 
+      v-if="user !== null && user.isAdmin" 
+      @click="navigateTo('/manage-users')" 
+      :class="{ active: route.name === 'admin-page' }"
+    >
+      <img class="users" src="../temporary-images/profile-user.png" />
+      <div class="ani_square"></div>
+    </div>
 
-  <div 
-    class="icon-container" 
-    v-if="user !== null && user.isAdmin" 
-    @click="logoutUser()">
-    <img class="users" src="../temporary-images/logout.png" />
-    <div class="ani_square"></div>
+    <div 
+      class="icon-container" 
+      v-if="user !== null && user.isAdmin" 
+      @click="logoutUser()">
+      <img class="users" src="../temporary-images/logout.png" />
+      <div class="ani_square"></div>
+    </div>
   </div>
-</div>
 
 </template>
