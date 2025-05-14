bf35d58b6ec76b3fe8de74a696fddbf7cd4c1886
Maksym Zinchenko <awesome20051969@gmail.com>
2025-05-14 18:39:03 +0200

    Mobile-Nav ist fertig

--- client/src/components/elements/Mobile-Navigation-Bar.vue
+++ client/src/components/elements/Mobile-Navigation-Bar.vue
@@ -13,6 +13,16 @@ function navigateTo(path: string): void {
 
 const user = ref<any|null>(null);
 
+async function logoutUser()  {
+  try {
+    const response = await api.post('/login/logout', {}, { withCredentials: true })
+    console.log(response.data.message)
+    router.push('/')
+  } catch (error: any) {
+    console.error('Logout failed', error.response?.data)
+  }
+}
+
 onMounted(async() => {
     const response = await api.get('/auth', { withCredentials: true });
     user.value  = response.data.user;
@@ -58,6 +68,14 @@ onMounted(async() => {
     :class="{ active: route.name === 'admin-page' }"
   >
     <img class="users" src="../temporary-images/profile-user.png" />
+    <div class="ani_square"></div>
+  </div>
+
+  <div 
+    class="icon-container" 
+    v-if="user !== null && user.isAdmin" 
+    @click="logoutUser()">
+    <img class="users" src="../temporary-images/logout.png" />
     <div class="ani_square"></div>
   </div>
 </div>
