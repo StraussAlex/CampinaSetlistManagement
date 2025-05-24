dd7d37090adc5b8f48d816ef4570f615f6fc47ac
Maksym Zinchenko <awesome20051969@gmail.com>
2025-05-24 15:47:39 +0200

    Login Page styling wurde hinzugefügt

--- client/src/components/login.vue
+++ client/src/components/login.vue
@@ -42,7 +42,11 @@ async function loginUser(): Promise<void> {
 </script>
 
 <template>
-  <mobile-header></mobile-header>
+  <div class="app">
+  <div class="header-wrapper">
+    <mobile-header></mobile-header>
+  </div>
+  <div class = "shadow-wrapper">
   <div id="login-centering" class="flex">
     <div>
       <h1>Login</h1>
@@ -65,16 +69,59 @@ async function loginUser(): Promise<void> {
             placeholder="password123"
         />
       </label>
-      <br/>
-      <button @click="loginUser" class="btn-primary">Log In</button>
+        <br/>
+        <button @click="loginUser" class="btn-primary">Log In</button>
+      </div>
     </div>
   </div>
-  <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+    <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
+  </div>
 </template>
 
 <style>
   #login-centering{
     height: 70vh;
     justify-content: space-around;
+  }
+
+  .section-heading{
+    background: none;
+  }
+  .app {
+    background-image: url("../assets/Icons/landingPageBackground.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: left;
+    background-position-y: bottom;
+    overflow-x: hidden;
+    position: fixed;
+    min-height: 100vh; 
+    width: 100%;
+  }
+
+  .shadow-wrapper{
+    background: linear-gradient(
+    180deg,
+    color-mix(in srgb, var(--background) 100%, transparent) 0%,
+    color-mix(in srgb, var(--background) 98%, transparent) 10%,
+    color-mix(in srgb, var(--background) 96%, transparent) 20%,
+    color-mix(in srgb, var(--background) 93%, transparent) 30%,
+    color-mix(in srgb, var(--background) 88%, transparent) 40%,
+    color-mix(in srgb, var(--background) 80%, transparent) 50%,
+    color-mix(in srgb, var(--background) 70%, transparent) 60%,
+    color-mix(in srgb, var(--background) 60%, transparent) 70%,
+    color-mix(in srgb, var(--background) 40%, transparent) 80%,
+    color-mix(in srgb, var(--background) 20%, transparent) 90%,
+    color-mix(in srgb, var(--background) 0%, transparent) 100%
+  );
+    padding-bottom: 300px;
+  }
+
+  .h1,h2,p{
+    text-shadow: var(--background) 2px 2px 20px;
+  }
+
+  .header-wrapper{
+    background-color: var(--background);
   }
 </style>
